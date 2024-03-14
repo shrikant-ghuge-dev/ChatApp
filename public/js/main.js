6fc063e479a334bedc74284150dbe0e3f6368904
Shrikant Ghuge <shreeghuge1@gmail.com>
2024-03-14 22:35:57 +0530

    Basic chat between users

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,26 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages')
+
 const socket = io();
 
 socket.on('message', message => {
-    console.log('asdsa', message)
+    console.log(message)
+    displayMessage(message);
+
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 })
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = e.target?.elements?.msg?.value;
+    console.log(msg)
+    socket.emit('chatMessage', msg)
+})
+
+function displayMessage(msg) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Shree <span>&nbsp;9:12pm</span></p>
+    <p class="text">${msg}</p>`;
+    document.querySelector('.chat-messages')?.appendChild(div)
+}

2984db9a183b4e438f931610f27d9868f5c0c7ce
Shrikant Ghuge <shreeghuge1@gmail.com>
2024-03-14 21:52:40 +0530

    Basic setup

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const path = require('path')
 const app = express()
-const PORT = 300 || process.env.PORT
+const server = http.createServer(app)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const io = socketio(server)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', socket => {
+
+    socket.broadcast.emit('message', 'A user has joined the chat')
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+})
+const PORT = 3000 || process.env.PORT
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
